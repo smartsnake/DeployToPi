799c544c418d3748789779c379de5fd0fe5062af
Brian Dearing <smartsnake1@gmail.com>
2020-01-01 12:04:46 -0600

    Incomplete update
    Added logging
    Adding multiple repo support
    -Currently broken

--- main.py
+++ main.py
@@ -1,69 +1,94 @@
 from git import Repo
 import os, shutil
-#import docker
+import logging
 import subprocess
+import threading
+import json
+import sys
+import time
 
-DIR_NAME = "Test"
+def setup(config):
+    DIR_NAME = "Test"
+    logging.info(f"Thread {config['DockerImageName']}    : Started thread.")
+    #Change this to whatever docker repo on github
+    REMOTE_URL = config['REMOTE_URL']#'https://github.com/smartsnake/DeploymentTest.git'
+    latestCommitFile = config['DockerImageName'] + config['latestCommitFile']#'latest.commit'
+    WorkingDir = config['WorkingDir']#'Deploy'
+    DockerImageName = config['DockerImageName']#'program'
+    latestCommit = config['latestCommit']#None
 
-#Change this to whatever docker repo on github
-REMOTE_URL = 'https://github.com/smartsnake/DeploymentTest.git'
-latestCommitFile = 'latest.commit'
-WorkingDir = 'Deploy'
-DockerImageName = 'program'
+    debug = True
 
-debug = True
-
-if os.path.isdir(DIR_NAME):
-    shutil.rmtree(DIR_NAME)
-os.mkdir(DIR_NAME)
-
-latestCommit = None
-
-if os.path.exists(latestCommitFile):
-    latestCommit = open(latestCommitFile, 'r').read()
-else:
-    repo = Repo.clone_from(REMOTE_URL, DIR_NAME)#Location of git project that needs
-    latestCommit = repo.head.commit
-    thing= open(latestCommitFile, 'w')
-    thing.write(str(latestCommit))
-    thing.close()
-
-print(str(latestCommit))
-print('Checking for new commits...')
-while True:
     if os.path.isdir(DIR_NAME):
         shutil.rmtree(DIR_NAME)
     os.mkdir(DIR_NAME)
-    repo = Repo.clone_from(REMOTE_URL, DIR_NAME)#Location of git project that needs
-    newCommit = repo.head.commit
-    if str(newCommit) != str(latestCommit):
-        print('Updating Program!')
-        print(f'newCommit: {newCommit}, latestCommit: {latestCommit}')
-        if os.path.isdir(WorkingDir):
-            shutil.rmtree(WorkingDir)
-        os.mkdir(WorkingDir)
-        Repo.clone_from(REMOTE_URL, WorkingDir)
-        thing= open(latestCommitFile, 'w')
-        thing.write(str(newCommit))
-        thing.close()
+
+    # if os.path.exists(latestCommitFile):
+    #     latestCommit = open(latestCommitFile, 'r').read()
+    if latestCommit == 'None':
+        repo = Repo.clone_from(REMOTE_URL, DIR_NAME)#Location of git project that needs
         latestCommit = repo.head.commit
+        thing = open(latestCommitFile, 'w')
+        thing.write(str(latestCommit))
+        thing.close()
+    else:
+        pass
 
-        if debug:
-            print('Deleting old container')
-        deletingContainer = subprocess.call(['docker', 'rm', '-f', f'{DockerImageName}'])
+    logging.info(f"Thread {config['DockerImageName']}    : latest commit = {str(latestCommit)}")
+    logging.info(f"Thread {config['DockerImageName']}    : Checking for new commits...")
 
-        if debug:
-            print('Building image...')
-        build = subprocess.call(['docker', 'build', '-t', f'{DockerImageName}', f'{WorkingDir}/'])
-        
-        if debug:
-            print('Running image...')
-        program = subprocess.call(['docker', 'run', f'--name={DockerImageName}', f'{DockerImageName}'])
+    while True:
+        if os.path.isdir(DIR_NAME):
+            shutil.rmtree(DIR_NAME)
+        os.mkdir(DIR_NAME)
+        repo = Repo.clone_from(REMOTE_URL, DIR_NAME)#Location of git project that needs
+        newCommit = repo.head.commit
+        if str(newCommit) != str(latestCommit):
+            logging.info(f"Thread {config['DockerImageName']}    : Updating Program!")
+            logging.info(f"Thread {config['DockerImageName']}    : newCommit: {newCommit}, latestCommit: {latestCommit}")
+            if os.path.isdir(WorkingDir):
+                shutil.rmtree(WorkingDir)
+            os.mkdir(WorkingDir)
+            Repo.clone_from(REMOTE_URL, WorkingDir)
+            thing= open(latestCommitFile, 'w')
+            thing.write(str(newCommit))
+            thing.close()
+            latestCommit = repo.head.commit
 
-        logs = subprocess.Popen(['docker', 'logs', '-f', f'{DockerImageName}'],
-                        stdout=subprocess.PIPE)
+            if debug:
+                logging.info(f"Thread {config['DockerImageName']}    : Deleting old container")
+            deletingContainer = subprocess.call(['docker', 'rm', '-f', f'{DockerImageName}'])
 
-        print(f'{DockerImageName} is deployed.')
-        print('Checking for new commits...')
+            if debug:
+                logging.info(f"Thread {config['DockerImageName']}    : Building image...")
+            build = subprocess.call(['docker', 'build', '-t', f'{DockerImageName}', f'{WorkingDir}/'])
+            
+            if debug:
+                logging.info(f"Thread {config['DockerImageName']}    : Running image...")
+            program = subprocess.call(['docker', 'run', f'--name={DockerImageName}', f'{DockerImageName}'])
+
+            logs = subprocess.Popen(['docker', 'logs', '-f', f'{DockerImageName}'],
+                            stdout=subprocess.PIPE)
+
+            logging.info(f"Thread {config['DockerImageName']}    : {DockerImageName} is deployed.")
+            logging.info(f"Thread {config['DockerImageName']}    : Checking for new commits...")
 
 
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    logging.info("Main    : before loading config")
+
+    with open('config.json') as json_data_file:
+        config = json.load(json_data_file)
+
+    if config == None:
+        logging.info('Main    : Please create a config.json file.')
+        sys.exit()
+    else:
+        logging.info("Main    : loaded config.")
+        for thing in config:
+            x = threading.Thread(target=setup, args=(thing,))
+
+            logging.info("Main    : Starting thread.")
+            x.start()
